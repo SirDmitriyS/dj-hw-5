f935f9e1994c24c3fb120dd7857a4470d7fe76f1
Dmitriy Siranchuk <sdmitriys@mail.ru>
2023-11-26 14:39:32 +0300

    добавлена работа с датчиками и их измерениями

--- measurement/serializers.py
+++ measurement/serializers.py
@@ -1,3 +1,27 @@
 from rest_framework import serializers
 
-# TODO: опишите необходимые сериализаторы
+from measurement.models import Measurement, Sensor
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
+
+class MeasurementFullSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['id', 'sensor', 'temperature', 'created_at']
+
+
+class MeasurementSerializer(MeasurementFullSerializer):
+    class Meta(MeasurementFullSerializer.Meta):
+        fields = ['temperature', 'created_at']
+
+
+class SensorDetailSerializer(SensorSerializer):
+    measurements = MeasurementSerializer(read_only=True, many=True)
+
+    class Meta(SensorSerializer.Meta):
+        fields = SensorSerializer.Meta.fields + ['measurements']
